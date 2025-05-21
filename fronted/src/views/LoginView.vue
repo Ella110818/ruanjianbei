5213c30ecfa8792d9e522e9b51b322ead6fa2060
Ella110818 <3191674615@qq.com>
2025-05-21 14:56:47 +0800

    denglugai

--- fronted/src/views/LoginView.vue
+++ fronted/src/views/LoginView.vue
@@ -31,7 +31,7 @@
                 v-model="formData.loginAccount" 
                 :class="{'error': errors.loginAccount}"
                 @blur="validateField('loginAccount')"
-                placeholder="请输入用户名"
+                placeholder="请输入用户名111"
                 required
               >
             </div>
