391457b97f062c3b50b2ba83722bdf6bdaa8ba8b
Ella110818 <3191674615@qq.com>
2025-07-08 16:38:08 +0800

    后端改了

--- fronted/src/api/index.js
+++ fronted/src/api/index.js
@@ -5,7 +5,7 @@ const ENV = {
         API_VERSION: 'api'
     },
     production: {
-        API_URL: 'https://29c44f27bbdc.ngrok-free.app',  // 更新为新的ngrok地址
+        API_URL: 'https://b0642ff316d7.ngrok-free.app',  // 更新为新的ngrok地址
         API_VERSION: 'api'
     }
 };
