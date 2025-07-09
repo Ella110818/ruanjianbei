7d67b825a1bef7a81c5ceee864af26eecd40673d
Ella110818 <3191674615@qq.com>
2025-07-09 14:54:07 +0800

    部署

--- fronted/vue.config.js
+++ fronted/vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  productionSourceMap: false,  // 生产环境不生成 source map
+  publicPath: './',  // 使用相对路径
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 250000,
+      }
+    }
+  },
+  css: {
+    extract: true,  // 是否提取 CSS 到单独的文件
+    sourceMap: false,  // 生产环境不生成 CSS source map
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/variables.scss";`  // 全局 SASS 变量
+      }
+    }
+  }
 })
