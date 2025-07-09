8f0163ec0abd696eb75f1301b522c81ef1b95151
Ella110818 <3191674615@qq.com>
2025-07-09 16:42:55 +0800

    二次测试

--- fronted/vue.config.js
+++ fronted/vue.config.js
@@ -6,8 +6,28 @@ module.exports = defineConfig({
   productionSourceMap: false,
   configureWebpack: {
     output: {
-      filename: '[name].[hash].js',
-      chunkFilename: '[name].[hash].js'
+      filename: 'js/[name].[hash].js',
+      chunkFilename: 'js/[name].[hash].js'
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendors: {
+            name: 'chunk-vendors',
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            chunks: 'initial'
+          },
+          common: {
+            name: 'chunk-common',
+            minChunks: 2,
+            priority: -20,
+            chunks: 'initial',
+            reuseExistingChunk: true
+          }
+        }
+      }
     }
   }
 })
