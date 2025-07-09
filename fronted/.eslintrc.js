bde95ddaed66826fc5f6c14213aaa692016004ff
Ella110818 <3191674615@qq.com>
2025-07-09 14:59:30 +0800

    部署2

--- fronted/.eslintrc.js
+++ fronted/.eslintrc.js
@@ -9,6 +9,12 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
   parserOptions: {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
