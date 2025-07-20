9cdac59f86ea5a900de1e3ec6bf5629dda6556ad
Ella110818 <3191674615@qq.com>
2025-07-20 15:58:16 +0800

    rt

--- fronted/src/api/index.js
+++ fronted/src/api/index.js
@@ -198,7 +198,7 @@ export const getExercises = (params) => instance.get('/exercises/', { params })
 export const createExercise = (data) => instance.post('/exercises/', data)
 export const generateQuestions = (data) => instance.post('/exercises/generate/', data)
 export const submitStudentAnswer = (data) => instance.post('/student-answers/', data)
-export const getMyCourses = () => instance.get('/courses/my-courses/')
+export const getMyCourses = () => instance.get('/courses/my_courses/')
 
 // 获取当前用户信息
 export const getCurrentUser = async () => {
